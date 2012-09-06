8902bde3fd3da24d90dba83eea1f8053150453b0
Yangmun Choi <yangmungi@ucla.edu>
2012-09-06 15:15:04 -0700

    Renaming bash scripts to distinguish functions.

--- git-rec-commit.sh
+++ git-rec-commit.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 inittime=`date`
+if [ -z "$2" ]
+then
+    echo "$0 <Times run> <Git remote repo>"
+    exit
+fi
+    
 for i in `seq $1`
 do
     /usr/bin/git add -A \
         && /usr/bin/git commit -m "Automated recursive git repository work $i - started $inittime." \
-        && /usr/bin/git push origin
+        && /usr/bin/git push $2
 done
---
+++ git-rec-mirror-gen.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+    echo "Needs remote name."
+    exit 1
+fi
+/usr/bin/git clone --mirror ./rec-mirror.git
+/usr/bin/git remote add $1 ./rec-mirror.git
