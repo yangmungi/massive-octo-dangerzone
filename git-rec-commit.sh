6e1c9b8cbeb5c11f9e11cbaba295557a5b21bd2f
Yangmun Choi <yangmungi@ucla.edu>
2014-03-02 10:39:00 -0800

    Making recursive git commit restricted

--- git-rec-commit.sh
+++ git-rec-commit.sh
@@ -2,13 +2,13 @@
 inittime=`date`
 if [ -z "$2" ]
 then
-    echo "$0 <Times run> <Git remote repo>"
+    echo "$0 <times run> <git remote>"
     exit
 fi
     
 for i in `seq $1`
 do
-    /usr/bin/git add -A \
+    /usr/bin/git add ./rec-mirror.git \
         && /usr/bin/git commit -m "Automated recursive git repository work $i - started $inittime." \
         && /usr/bin/git push $2
 done
